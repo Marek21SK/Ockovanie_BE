a4f1d9adbe920105f31a2c7d67c525c61f07dbc5
mirka <mirka.syc@gmail.com>
2022-04-09 15:36:52 +0200

    Vakcinácia

---
+++ src/main/java/MDMMDM/demo/VakcinaciaDto.java
@@ -0,0 +1,58 @@
+package MDMMDM.demo;
+
+public class VakcinaciaDto {
+    private Long id;
+    private Long vakcinaId;
+    private Long osobaId;
+    private String nazovVakciny;
+    private String menoOsoba;
+    private String priezviskoOsoba;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getVakcinaId() {
+        return vakcinaId;
+    }
+
+    public void setVakcinaId(Long vakcinaId) {
+        this.vakcinaId = vakcinaId;
+    }
+
+    public Long getOsobaId() {
+        return osobaId;
+    }
+
+    public void setOsobaId(Long osobaId) {
+        this.osobaId = osobaId;
+    }
+
+    public String getNazovVakciny() {
+        return nazovVakciny;
+    }
+
+    public void setNazovVakciny(String nazovVakciny) {
+        this.nazovVakciny = nazovVakciny;
+    }
+
+    public String getMenoOsoba() {
+        return menoOsoba;
+    }
+
+    public void setMenoOsoba(String menoOsoba) {
+        this.menoOsoba = menoOsoba;
+    }
+
+    public String getPriezviskoOsoba() {
+        return priezviskoOsoba;
+    }
+
+    public void setPriezviskoOsoba(String priezviskoOsoba) {
+        this.priezviskoOsoba = priezviskoOsoba;
+    }
+}
