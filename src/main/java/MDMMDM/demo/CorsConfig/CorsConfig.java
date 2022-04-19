893e6cec6af5b5126d9a6b4d7947816c4add9c79
Marek Pástor <marekpastorml@gmail.com>
2022-04-19 13:13:10 +0200

    Moved to the package.

--- src/main/java/MDMMDM/demo/CorsConfig/CorsConfig.java
+++ src/main/java/MDMMDM/demo/CorsConfig/CorsConfig.java
@@ -1,4 +1,4 @@
-package MDMMDM.demo;
+package MDMMDM.demo.CorsConfig;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
